6fbc8fe718788fc80b9b12eedac2f6e3ad5e0e55
Mena Maged <menaxmaged@gmail.com>
2023-03-12 04:17:53 +0200

    FiXUP

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ else
 fi
 '
 if [ "$UID" -eq "$ROOT_UID" ]; then
- echo "Please run this script as a normal user"
+ echo "Please run this script as a nosudo al user"
  exit
 fi
 CONF_DIR="$HOME/.config/"
@@ -37,30 +37,29 @@ SRC_DIR=$(cd $(dirname $0) && pwd)
 THEME_NAME=Ventura
 LATTE_DIR="$HOME/.config/latte"
 
-[[ ! -d ${AURORAE_DIR} ]] && mkdir -p ${AURORAE_DIR}
-[[ ! -d ${SCHEMES_DIR} ]] && mkdir -p ${SCHEMES_DIR}
-[[ ! -d ${PLASMA_DIR} ]] && mkdir -p ${PLASMA_DIR}
-[[ ! -d ${LOOKFEEL_DIR} ]] && mkdir -p ${LOOKFEEL_DIR}
-[[ ! -d ${KVANTUM_DIR} ]] && mkdir -p ${KVANTUM_DIR}
-[[ ! -d ${WALLPAPER_DIR} ]] && mkdir -p ${WALLPAPER_DIR}
-[[ ! -d ${Cusrsor_DIR} ]] && mkdir -p ${Cusrsor_DIR}
-[[ ! -d ${Icons_DIR} ]] && mkdir -p ${Icons_DIR}
-[[ ! -d ${PLASMOID_DIR} ]] && mkdir -p ${PLASMOID_DIR}
-
 cp -rf "${SRC_DIR}"/configs/Xresources "$HOME"/.Xresources
 
 install_icons(){
-git clone git@github.com:menaxmaged/MacOS-icon-theme.git
+git clone https://github.com/menaxmaged/MacOS-icon-theme.git
 cd MacOS-icon-theme
-./install.sh
+sudo ./install.sh
 cd ..
 
 }
 
 install_gtk(){
-git clone git@github.com:menaxmaged/MacOS-gtk-theme.git
+git clone https://github.com/menaxmaged/MacOS-gtk-theme.git
 cd MacOS-gtk-theme
-./install.sh
+sudo ./install.sh
+cd ..
+
+}
+
+
+install_sddm(){
+
+cd sddm
+sudo ./install.sh
 cd ..
 
 }
@@ -78,43 +77,49 @@ install() {
   WALLPAPER_DIR="/usr/share/wallpapers"
   Cusrsor_DIR="/usr/share/icons"
   Icons_DIR="/usr/share/icons"
+ fonts_DIR="/usr/share/fonts/noto/"
+ [[ ! -d ${AURORAE_DIR} ]] && sudo mkdir -p ${AURORAE_DIR}
+ [[ ! -d ${SCHEMES_DIR} ]] && sudo mkdir -p ${SCHEMES_DIR}
+ [[ ! -d ${PLASMA_DIR} ]] && sudo mkdir -p ${PLASMA_DIR}
+ [[ ! -d ${LOOKFEEL_DIR} ]] && sudo mkdir -p ${LOOKFEEL_DIR}
+ [[ ! -d ${KVANTUM_DIR} ]] && sudo mkdir -p ${KVANTUM_DIR}
+ [[ ! -d ${WALLPAPER_DIR} ]] && sudo mkdir -p ${WALLPAPER_DIR}
+ [[ ! -d ${Cusrsor_DIR} ]] && sudo mkdir -p ${Cusrsor_DIR}
+ [[ ! -d ${Icons_DIR} ]] && sudo mkdir -p ${Icons_DIR}
+ [[ ! -d ${PLASMOID_DIR} ]] && sudo mkdir -p ${PLASMOID_DIR}
 
-  [[ -d ${AURORAE_DIR}/${name} ]] && rm -rf ${AURORAE_DIR}/${name}*
-  [[ -d ${PLASMA_DIR}/${name} ]] && rm -rf ${PLASMA_DIR}/${name}*
-  [[ -f ${SCHEMES_DIR}/${name}.colors ]] && rm -rf ${SCHEMES_DIR}/${name}*.colors
-  [[ -d ${LOOKFEEL_DIR}/com.github.mena.${name} ]] && rm -rf ${LOOKFEEL_DIR}/com.github.mena.${name}*
-  [[ -d ${KVANTUM_DIR}/${name} ]] && rm -rf ${KVANTUM_DIR}/${name}*
-  [[ -d ${WALLPAPER_DIR}/${name} ]] && rm -rf ${WALLPAPER_DIR}/${name}*
-  [[ -f ${LATTE_DIR}/${name}.layout.latte ]] && rm -rf ${LATTE_DIR}/${name}.layout.latte
 
-  cp -r ${SRC_DIR}/aurorae/*                                                         ${AURORAE_DIR}
-  cp -r ${SRC_DIR}/Kvantum/*                                                         ${KVANTUM_DIR}
-  cp -r ${SRC_DIR}/color-schemes/*                                                   ${SCHEMES_DIR}
-  cp -r ${SRC_DIR}/plasma/desktoptheme/${name}*                                      ${PLASMA_DIR}
-  cp -r ${SRC_DIR}/plasma/desktoptheme/icons                                         ${PLASMA_DIR}/${name}
-  cp -r ${SRC_DIR}/plasma/desktoptheme/icons                                         ${PLASMA_DIR}/${name}-dark
-  cp -r ${SRC_DIR}/plasma/look-and-feel/*                                            ${LOOKFEEL_DIR}
-  cp -r ${SRC_DIR}/plasma/plasmoids/*                                                ${PLASMOID_DIR}
-  cp -r ${SRC_DIR}/wallpaper/*                                                       ${WALLPAPER_DIR}
-  cp -r ${SRC_DIR}/icons/cursors/*                                                   ${Cusrsor_DIR}
+ [[ -d ${AURORAE_DIR}/${name} ]] && sudo rm -rf ${AURORAE_DIR}/${name}*
+ [[ -d ${PLASMA_DIR}/${name} ]] && sudo rm -rf ${PLASMA_DIR}/${name}*
+ [[ -f ${SCHEMES_DIR}/${name}.colors ]] && sudo rm -rf ${SCHEMES_DIR}/${name}*.colors
+ [[ -d ${LOOKFEEL_DIR}/com.github.mena.${name} ]] && sudo rm -rf ${LOOKFEEL_DIR}/com.github.mena.${name}*
+ [[ -d ${KVANTUM_DIR}/${name} ]] && sudo rm -rf ${KVANTUM_DIR}/${name}*
+ [[ -d ${WALLPAPER_DIR}/${name} ]] && sudo rm -rf ${WALLPAPER_DIR}/${name}*
+  [[ -f ${LATTE_DIR}/${name}.layout.latte ]] && sudo rm -rf ${LATTE_DIR}/${name}.layout.latte
 
-  cp -r ${SRC_DIR}/gtk/themes/*                                                      ${GTK_DIR}
-# install_icons
- # cp -r ${SRC_DIR}/icons/icons/*                                                     ${Icons_DIR}
-  #cp -r ${SRC_DIR}/fonts/*                                                           ${fonts_DIR}
+sudo  cp -r ${SRC_DIR}/aurorae/*                                                         ${AURORAE_DIR}
+sudo  cp -r ${SRC_DIR}/Kvantum/*                                                         ${KVANTUM_DIR}
+sudo  cp -r ${SRC_DIR}/color-schemes/*                                                   ${SCHEMES_DIR}
+sudo  cp -r ${SRC_DIR}/plasma/desktoptheme/${name}*                                      ${PLASMA_DIR}
+sudo  cp -r ${SRC_DIR}/plasma/desktoptheme/icons                                         ${PLASMA_DIR}/${name}
+sudo  cp -r ${SRC_DIR}/plasma/desktoptheme/icons                                         ${PLASMA_DIR}/${name}-dark
+sudo  cp -r ${SRC_DIR}/plasma/look-and-feel/*                                            ${LOOKFEEL_DIR}
+sudo  cp -r ${SRC_DIR}/plasma/plasmoids/*                                                ${PLASMOID_DIR}
+sudo  cp -r ${SRC_DIR}/wallpaper/*                                                       ${WALLPAPER_DIR}
+sudo  cp -r ${SRC_DIR}/icons/cursors/*                                                   ${Cusrsor_DIR}
+sudo  cp -r ${SRC_DIR}/fonts/*                                                           ${fonts_DIR}
  # cp -r ${SRC_DIR}/fonts/NotoColorEmoji.ttf                                          ${EMOJIS_DIR}
   cp -r ${SRC_DIR}/confs/*                                                            ${CONF_DIR}
-  [[ -d ${LATTE_DIR} ]] && cp -r ${SRC_DIR}/latte-dock/*                             ${LATTE_DIR}
 }
 
 echo "Installing '${THEME_NAME} kde themes'..."
 
-sudo install "${name:-${THEME_NAME}}"
-sudo install_icons
-sudo install_gtk
+install "${name:-${THEME_NAME}}"
+install_icons
+install_gtk
+install_sddm
 sudo add-apt-repository ppa:papirus/papirus
-sudo apt install webhttrack libgtkmm-3.0-1v5 libcdio-paranoia2 qt5-style-kvantum qt5-style-kvantum-themes libgtk2.0-0
-sudo apt update && apt upgrade -y
+sudo apt install libgtkmm-3.0-1v5 libcdio-paranoia2 qt5-style-kvantum qt5-style-kvantum-themes libgtk2.0-0 latte-dock
 cp -r ${SRC_DIR}/confs/*                                                            ${CONF_DIR}
 
 
